e8d3828e639c4c7348f917e32618cb9d2f5f28a9
Lucieno zandry <lucienozandry4@gmail.com>
2025-04-04 13:34:32 +0300

    Préparation pour la mise en production

--- src/app.js
+++ src/app.js
@@ -26,6 +26,6 @@ app.get('/users', async (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('🚀 Server is running on http://localhost:3000')
 });
